954318d3248cbb03c5da3c6d2ad60325a3a493ac
maiklzak <maiklzak@gmail.com>
2021-04-30 16:07:26 +0300

    add spring-fronted, thymeleaf, delete table author

--- src/main/java/com/example/MyBookShopApp/controllers/MainPageController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/MainPageController.java
@@ -1,40 +1,31 @@
 package com.example.MyBookShopApp.controllers;
 
-import com.example.MyBookShopApp.data.AuthorService;
+import com.example.MyBookShopApp.data.Book;
 import com.example.MyBookShopApp.data.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
 
 @Controller
-@RequestMapping("/bookshop")
 public class MainPageController {
 
     private final BookService bookService;
-    private final AuthorService authorService;
+
+    @ModelAttribute("recommendedBooks")
+    public List<Book> recommendedBooks() {
+        return bookService.getBookData();
+    }
 
     @Autowired
-    public MainPageController(BookService bookService, AuthorService authorService) {
+    public MainPageController(BookService bookService) {
         this.bookService = bookService;
-        this.authorService = authorService;
     }
 
-    @GetMapping("/main")
-    public String mainPage(Model model) {
-        model.addAttribute("bookData", bookService.getBookData());
+    @GetMapping("/")
+    public String mainPage() {
         return "index";
-    }
-
-    @GetMapping("/genres")
-    public String genresPage() {
-        return "genres/index";
-    }
-
-    @GetMapping("/authors")
-    public String authorsPage(Model model) {
-        model.addAttribute("authors", authorService.getAuthors());
-        return "authors/index";
     }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,4 @@
-DROP TABLE IF EXISTS authors;
 DROP TABLE IF EXISTS books;
-
-CREATE TABLE authors
-(
-    id          INT AUTO_INCREMENT PRIMARY KEY,
-    first_name  VARCHAR(250) NOT NULL,
-    second_name VARCHAR(250) NOT NULL
-);
 
 CREATE TABLE books
 (
@@ -14,8 +6,6 @@ CREATE TABLE books
     author     VARCHAR(250) NOT NULL,
     title     VARCHAR(250) NOT NULL,
     priceOld  VARCHAR(250) DEFAULT NULL,
-    price     VARCHAR(250) DEFAULT NULL,
-    author_id INT,
-    FOREIGN KEY (author_id) REFERENCES authors (id)
+    price     VARCHAR(250) DEFAULT NULL
 );
 
