7b40b1b3f74611181e2b767f1106ff0a14d0c0a1
maiklzak <maiklzak@gmail.com>
2021-08-06 23:45:42 +0300

    selenium tests

--- src/test/resources/scripts-test/test-data-after.sql
+++ src/test/resources/scripts-test/test-data-after.sql
@@ -4,4 +4,5 @@ DELETE FROM user_role WHERE user_id = 5005;
 DELETE FROM book2user WHERE user_id = 5005;
 DELETE FROM message WHERE user_id = 5005;
 DELETE FROM balance_transaction WHERE user_id = 5005;
+DELETE FROM book_review WHERE user_id = 5005;
 DELETE FROM users WHERE id = 5005;
